c0540ea38e8d733b2259d775dd24107d7162f7af
Sanna Nan <nabobiix@gmail.com>
2025-01-27 11:21:15 +0700

    i honestly don't know what i am doing anymore

--- src/components/VideoHighlight.jsx
+++ src/components/VideoHighlight.jsx
@@ -14,7 +14,14 @@ const VideoHighlight = () => {
 
       {/* Video Section */}
       <div className="lg:w-[62.5%] flex justify-center w-full">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/6vo9tFsUn8s?si=cuQz5uZQhXzZ__Ko" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+        <iframe
+          className="w-full h-64 lg:h-96 rounded-lg shadow-lg"
+          src="https://www.youtube.com/embed/WeA7edXsU40"
+          title="Hubble's Highlights from its 34th Year in Orbit"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
